a20b0d2d732b40b7bb6df695c849d3afc37571b3
Rex-Sanchez <r3x.sanch3z@gmail.com>
2024-08-08 23:25:12 +0200

    made it into one struct instead of 2
    SocketServer and SocketClient are now one struct IPCSocket
    it now contains a new_server and a new_client constructor

--- src/lib.rs
+++ src/lib.rs
@@ -10,121 +10,106 @@ mod error;
 
 pub use crate::error::Result;
 
-pub struct SocketServer {
+pub struct IPCSocket {
     socket: UnixStream,
     addr: &'static str,
 }
 
-impl SocketServer {
-    pub fn new(addr: &'static str) -> Result<Self> {
+impl IPCSocket {
+    pub fn new_server(addr: &'static str) -> Result<Self> {
         let listener = UnixListener::bind(addr)?;
         let (socket, _) = listener.accept()?;
-
-        // some comment
         Ok(Self { socket, addr })
     }
-    pub fn recv<T: DeserializeOwned>(&mut self) -> Option<T> {
-        // create fixed 4 byte buffer;
+
+    pub fn new_client(addr: &'static str) -> Result<Self> {
+        let socket = UnixStream::connect(addr)?;
+        Ok(Self { socket, addr })
+    }
+
+    fn receive_data(&mut self, buffer: &mut [u8]) -> Result<Option<()>> {
+        if let Err(e) = self.socket.read_exact(buffer) {
+            if e.kind() == std::io::ErrorKind::UnexpectedEof {
+                // if the buffer is filled with 0x00 return a Ok(None);
+                return Ok(None);
+            } else {
+                return Err(e.into());
+            }
+        };
+        Ok(Some(()))
+    }
+    pub fn recv<T: DeserializeOwned>(&mut self) -> Result<Option<T>> {
         let mut buf = [0u8; 4];
 
-        // read 4 bytes of data;
-        self.socket.read_exact(&mut buf).ok()?;
+        if self.receive_data(&mut buf)?.is_none() {
+            return Ok(None);
+        }
 
-        // get len of message;
-        let message_len = u32::from_be_bytes(buf);
+        let mut message_buf = vec![0u8; u32::from_be_bytes(buf) as usize];
 
-        // create buffer with message len;
-        let mut message_buf = vec![0u8; message_len as usize];
+        if self.receive_data(&mut message_buf)?.is_none() {
+            return Ok(None);
+        }
 
-        // read from socket until buffer is full
-        self.socket.read_exact(&mut message_buf).ok()?;
+        let message = bincode::deserialize::<T>(&message_buf)?;
 
-        // deserialize the message
-        let message = bincode::deserialize::<T>(&message_buf).ok()?;
-
-        return Some(message);
+        return Ok(Some(message));
     }
-    pub fn recv_blocking<T: DeserializeOwned>(&mut self) -> T {
+
+    pub fn recv_blocking<T: DeserializeOwned>(&mut self) -> Result<T> {
         loop {
-            if let Some(message) = self.recv() {
-                return message;
+            if let Some(message) = self.recv()? {
+                return Ok(message);
             }
         }
     }
-    pub fn send<T: Serialize>(&mut self, message: T) -> Option<()> {
+
+    pub fn send<T: Serialize>(&mut self, message: T) -> Result<()> {
         // encode message;
-        let message = bincode::serialize(&message).ok()?;
+        let message = bincode::serialize(&message)?;
         // get len of message;
         let message_len = message.len();
 
         // send len of message;
-        self.socket.write_all(&message_len.to_be_bytes()).ok()?;
+        self.socket.write_all(&message_len.to_be_bytes())?;
         // send message;
-        self.socket.write_all(&message).ok()?;
+        self.socket.write_all(&message)?;
 
-        Some(())
+        Ok(())
     }
 }
 
-impl Drop for SocketServer {
+impl Drop for IPCSocket {
     fn drop(&mut self) {
         self.socket.shutdown(std::net::Shutdown::Both).ok();
-        remove_file(&self.addr).ok();
+        remove_file(self.addr).ok();
     }
 }
 
-pub struct SocketClient {
-    socket: UnixStream,
-}
 
-impl SocketClient {
-    pub fn new(addr: &str) -> crate::error::Result<Self> {
-        let socket = UnixStream::connect(addr)?;
 
-        Ok(Self { socket })
-    }
+#[cfg(test)]
+mod test {
+    use std::thread::spawn;
+    use crate::IPCSocket;
 
-    pub fn recv<T: DeserializeOwned>(&mut self) -> Option<T> {
-        // create a fixed size 4 byte buffer;
-        let mut buf = [0u8; 4];
+    #[test]
+    fn t1() {
+        let a = spawn(|| {
+            let mut s = IPCSocket::new_server("/tmp/sock.sock").unwrap();
+            let m: u32 = s.recv_blocking().unwrap();
+            assert_eq!(m, 69);
+            s.send::<u32>(42).ok();
+        });
 
-        // receiver message size;
-        self.socket.read_exact(&mut buf).ok()?;
+        let b = spawn(|| {
+            let mut c = IPCSocket::new_client("/tmp/sock.sock").unwrap();
+            c.send::<u32>(69).ok();
+            let m = c.recv_blocking::<u32>().unwrap();
+            assert_eq!(m, 42);
+        });
 
-        // convert buffer to u32;
-        let message_len = u32::from_be_bytes(buf);
-
-        // create buffer of size message size;
-        let mut message_buf = vec![0u8; message_len as usize];
-
-        // read from socket until buffer is full;
-        self.socket.read_exact(&mut message_buf).ok()?;
-
-        // deserialize the message into struct;
-        let message = bincode::deserialize::<T>(&message_buf).ok()?;
-
-        return Some(message);
-    }
-
-    pub fn recv_blocking<T: DeserializeOwned>(&mut self) -> T {
-        loop {
-            if let Some(message) = self.recv() {
-                return message;
-            }
-        }
-    }
-
-    pub fn send<T: Serialize>(&mut self, message: T) -> Option<()> {
-        // encode message;
-        let message = bincode::serialize(&message).ok()?;
-        // get len of message;
-        let message_len = (message.len() as u32).to_be_bytes();
-
-        // send len of message;
-        self.socket.write_all(&message_len).ok();
-        // send message;
-        self.socket.write_all(&message).ok();
-
-        Some(())
+        a.join().ok();
+        b.join().ok();
     }
 }
